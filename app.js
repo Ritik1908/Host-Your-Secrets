7d11089ed759ebb5443ba651d53969e146b5301c
Ritik <ritikverma8100775754@gmail.com>
2020-05-07 00:30:32 +0530

    Level 5: Authentications, passports added

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require('express-session')
+const passport = require("passport");
+const passportLocalMoongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,6 +17,18 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+app.use(session({
+	secret: "I am Ritik Verma.",
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -23,11 +37,15 @@ const userSchema = new mongoose.Schema({
 	password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['passowrd']});
+userSchema.plugin(passportLocalMoongoose);
 
+const User = new mongoose.model("user", userSchema)
 
-const user = new mongoose.model("user", userSchema)
-
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/", function(req, res){
 	res.render("home");
 });
@@ -37,39 +55,51 @@ app.get("/login", function(req, res){
 });
 
 app.post("/login", function(req, res){
-	const username = req.body.username;
-	const password = req.body.password;
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
 
-	user.findOne({email: username}, function(err, foundUser){
+	req.login(user, function(err){
 		if(err) {
 			console.log(err);
 		} else {
-			if(foundUser.password === password) {
-				res.render("secrets");
-			}
+			passport.authenticate("local")(req,res, function(){
+				res.redirect("/secrets");
+			});
 		}
 	})
-})
+});
 
 app.get("/register", function(req, res){
 	res.render("register");
 });
 
-app.post("/register", function(req, res){
-	const newUser = new user({
-		email: req.body.username,
-		password: req.body.password
-	});
-
-	newUser.save(function(err){
-		if(!err) {
-			res.render("secrets");
-		} else {
-			console.log(err);
-		}
-	})
+app.get("/secrets", function(req, res){
+	if(req.isAuthenticated()) {
+		res.render("secrets");
+	} else {
+		res.redirect("register");
+	}
 })
 
+app.post("/register", function(req, res){
+	User.register({username: req.body.username}, req.body.password, function(err, user){
+		if(err) {
+			console.log(err);
+			res.redirect("/login");
+		} else {
+			passport.authenticate("local")(req,res, function(){
+				res.redirect("/secrets");
+			});
+		}
+	});
+});
+
+app.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/");
+});
 
 app.listen(3000, function(){
 	console.log("Server started on port 3000");
